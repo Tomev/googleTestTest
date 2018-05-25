c492abe50306e9d0a1b3169cf63a431e393ee6b8
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-05-25 17:22:35 +0200

    [STABLE] gtest now added as a submodule instead of copied library.

---
+++ src/adder.cpp
@@ -0,0 +1,15 @@
+#include "adder.h"
+
+
+
+double adder::add(double x, double y)
+{
+  /** Add method with doubles as attributes. Returns sum of two doubles.
+   *
+   *  @brief Returns sum of two double addends.
+   *
+   *  @return x + y
+   */
+
+  return x + y;
+}
---
+++ src/adder.h
@@ -0,0 +1,11 @@
+#ifndef GOOGLETESTTEST_ADDER_H
+#define GOOGLETESTTEST_ADDER_H
+
+class adder
+{
+public:
+  static double add(double x, double y);
+private:
+};
+
+#endif //GOOGLETESTTEST_ADDER_H
